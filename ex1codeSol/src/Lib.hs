fd56b050ea9b477f2d2925fc88339a2a58777cf5
fishyFrogFace <camillmg@stud.ntnu.no>
2018-07-05 16:06:17 +0200

    Renamed project and added first function

---
+++ ex1codeSol/src/Lib.hs
@@ -0,0 +1,8 @@
+module Lib
+    ( add
+    ) where
+
+-- TASK 1
+-- Simple functions
+add :: Int -> Int -> Int
+add n m = n + m
---
+++ ex1codeSol/test/Spec.hs
@@ -0,0 +1,23 @@
+import Test.Hspec
+import Test.QuickCheck
+
+import Lib
+
+main :: IO ()
+main = hspec $ do
+
+    describe "add" $ do
+        it "can compute 2 + 2 = 4" $ do
+            add 2 2 `shouldBe` 4
+
+        it "has zero as a left identity" $ do
+            property $ \n -> add 0 n == n
+
+        it "has zero as a right identity" $ do
+            property $ \n -> add n 0 == n
+
+        it "is commutative" $ do
+            property $ \m n -> add m n == add n m
+
+        it "is associative" $ do
+            property $ \m n o -> add m (add n o) == add (add m n) o
