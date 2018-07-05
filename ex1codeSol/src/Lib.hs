d477b1b6073eae2daa334bc2c1ea8793cc59b1bb
fishyFrogFace <camillmg@stud.ntnu.no>
2018-07-05 16:35:19 +0200

    Fibonnacci numbers

--- ex1codeSol/src/Lib.hs
+++ ex1codeSol/src/Lib.hs
@@ -1,8 +1,23 @@
 module Lib
     ( add
+    , fib
     ) where
 
 -- TASK 1
 -- Simple functions
+
+-- finish the function "add" that takes two integers
+-- and returns the sum of them
 add :: Int -> Int -> Int
 add n m = n + m
+
+-- TASK 2
+-- Recursion
+
+-- finish the function "fib" that calculates the
+-- nth fibonnaci number 
+-- assuming that 0th = 0 and 1st = 1
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-2) + fib (n-1)
--- ex1codeSol/test/Spec.hs
+++ ex1codeSol/test/Spec.hs
@@ -3,6 +3,8 @@ import Test.QuickCheck
 
 import Lib
 
+fib21 = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765]
+
 main :: IO ()
 main = hspec $ do
 
@@ -21,3 +23,7 @@ main = hspec $ do
 
         it "is associative" $ do
             property $ \m n o -> add m (add n o) == add (add m n) o
+
+    describe "fib" $ do
+        it "can compute the first 21 fibonnaci numbers" $ do
+            map fib [0..20] `shouldBe` fib21
