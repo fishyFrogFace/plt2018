fb02188d2dc02007c220d2607a21ce1e3d46bcf2
fishyFrogFace <camillmg@stud.ntnu.no>
2018-07-27 01:15:56 +0200

    Add interpreter, remove list operations

--- ex2codeSol/src/Lib.hs
+++ ex2codeSol/src/Lib.hs
@@ -4,11 +4,11 @@ module Lib
     , splitOn
     , lex
     , tokenize
+    , interpret
     ) where
 
 import Prelude hiding (lex)
 import Data.Char (isDigit)
-import Data.Stack
 
 --TokErr is effectively a null type and won't be enforced by the type system
 --the programmer will need to check for it
@@ -54,3 +54,18 @@ tokenize lst = let tokens = map token lst
                 in case (filter (== TokErr) tokens) of
                     [] -> tokens
                     _  -> [TokErr]            
+
+calc :: [Token] -> Token -> [Token]
+calc (TokInt x:TokInt y:xs) (TokOp Plus)  = TokInt (x + y):xs
+calc (TokInt x:TokInt y:xs) (TokOp Minus) = TokInt (y - x):xs
+calc (TokInt x:TokInt y:xs) (TokOp Mult)  = TokInt (x * y):xs
+calc (TokInt x:TokInt y:xs) (TokOp Div)   = TokInt (y `div` x):xs
+calc lst tok                              = tok:lst
+
+--writing a folding function should be part of
+--last exercise or this one
+--at least talking about foldl/foldr and differences between them
+interpret :: [Token] -> [Token]
+interpret []       = []
+interpret [TokErr] = [TokErr]
+interpret lst      = foldl calc [] lst
--- ex2codeSol/test/Spec.hs
+++ ex2codeSol/test/Spec.hs
@@ -12,47 +12,6 @@ recList = [1..5] ++ recList
 
 main :: IO ()
 main = hspec $ do
-
-    describe "len" $ do
-        it "accurately computes the length of a list" $ do
-            property $ forAll genList $ \n -> len n == length n
-        it "returns 0 for empty list" $ do
-            len [] `shouldBe` 0
-    
-    describe "drop'" $ do
-        it "drops n items of a list with more than n items" $ do
-            drop' 5 [1..10] `shouldBe` [6..10]
-        it "drops the whole list when less than n items" $ do
-            drop' 47 ['a'..'z'] `shouldBe` []
-        it "returns whole list when n is negative" $ do
-            drop' (-3) "monkey" `shouldBe` "monkey"
-
-    describe "append" $ do
-        it "appends one list to another" $ do
-            property $ forAll genList $ \n m -> append n m == n ++ m
-        it "returns first list if second is empty" $ do
-            append [1..10] [] `shouldBe` [1..10]
-        it "returns second list if first list is empty" $ do
-            append [] [1..10] `shouldBe` [1..10]
-        it "returns empty list if both lists are empty" $ do
-            append [] [] `shouldBe` ([] :: [Int])
-    
-    describe "member" $ do
-        it "returns True if the element is in the list" $ do
-            member 'a' "salad" `shouldBe` True
-        it "retuns False if the element is not in the list" $ do
-            member 'a' "SALAD" `shouldBe` False
-        it "returns False if the list is empty" $ do
-            member 5 [] `shouldBe` False
-
-    describe "position" $ do
-        it "returns the position if the element is in the list" $ do
-            position 4 [0..10] `shouldBe` Just 4
-        it "returns Nothing if the element is not in the list" $ do
-            position 'a' ['b'..'z'] `shouldBe` (Nothing :: Maybe Int)
-        it "returns Nothing for empty list" $ do
-            position (Just 'a') [] `shouldBe` (Nothing :: Maybe Int)
-
     describe "splitOn" $ do
         it "splits a list of integers on a given integer" $ do
             take 3 (splitOn 1 recList) `shouldBe` replicate 3 [2,3,4,5]
@@ -74,3 +33,11 @@ main = hspec $ do
             property $ forAll genList $ \n -> tokenize [n] == [TokErr]
         it "returns empty list for empty input" $ do
             tokenize [] `shouldBe` ([] :: [Token])
+
+    describe "interpret" $ do
+        it "interprets a list of Tokens and returns the result" $ do
+            interpret [TokInt 230, TokInt 5, TokOp Plus, TokInt 10, TokOp Minus] `shouldBe` [TokInt 225]
+        it "returns empty list for empty input" $ do
+            interpret [] `shouldBe` ([] :: [Token])
+        it "returns error token for erroneous input" $ do
+            interpret [TokErr] `shouldBe` [TokErr]
