3f53a8b5b73a5ce72e25b2723824373b20ea6940
fishyFrogFace <camillmg@stud.ntnu.no>
2018-08-02 14:59:15 +0200

    Change main to infinite loop

--- ex2codeSol/app/Main.hs
+++ ex2codeSol/app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
 import Lib
+import Control.Monad
 
 parse :: String -> [Token]
 parse = interpret . tokenize . Lib.lex
 
 main :: IO ()
-main = getLine >>= print . parse
-    
+main = forever $ getLine >>= print . parse
+
+mainInfix :: IO ()
+mainInfix = undefined
