9bc91c49a57c212b31a90aaa419e70bbef0529e8
fishyFrogFace <camillmg@stud.ntnu.no>
2018-08-03 00:58:03 +0200

    Outline contents of exercise

--- ex2codeSol/src/Lib.hs
+++ ex2codeSol/src/Lib.hs
@@ -1,8 +1,40 @@
 module Lib
-    ( Tree(..)
-    , fun
+    ( drop'
+    , Tree(..)
     ) where
 
-data Tree = undefined
+-- TASK 1
+-- Type inference
 
-fun = undefined
+--what happens if we let the compiler decide the type signature?
+--drop' (Eq t, Num t) => t -> [a] -> [a]
+--why is this bad?
+drop' :: Int -> [a] -> [a]
+drop' _ []     = []
+drop' n (x:xs)
+    | n > 0     = drop' (n-1) xs
+    | otherwise = (x:xs)
+
+-- TASK 2
+-- Types, type classes and ad-hoc polymorphism
+
+-- create a type class
+-- create types that are instances of the type class
+
+data Tree a = undefined
+
+-- TASK 3
+-- Bounded parametric polymorphism
+
+-- make a function that ties together with the typeclass
+-- and types that are created
+
+-- usage of ad-hoc polymorphism
+
+-- TASK 4
+-- Pattern matching
+
+-- explore pattern matching in functions? maybe more heavy on theory?
+
+-- TASK 5
+-- Infinite lists and laziness
