813c410c750b2867ee417519305132033793dd63
fishyFrogFace <camillmg@stud.ntnu.no>
2018-08-05 20:41:14 +0200

    Complex instance of num, excluding signum

--- ex4codeSol/src/Lib.hs
+++ ex4codeSol/src/Lib.hs
@@ -22,11 +22,11 @@ instance Show Complex where
         
 instance Num Complex where
     (+) (Complex r1 i1) (Complex r2 i2) = Complex (r1+r2) (i1+i2)
-    (*) = undefined
-    abs = undefined
-    signum = undefined
-    fromInteger = undefined
-    (-) = undefined
+    (*) (Complex r1 i1) (Complex r2 i2) = Complex (r1*r2 - i1*i2) (r1*i2 + i1*r2)
+    abs (Complex r i) = Complex (sqrt (r**2+i**2)) 0
+    signum (Complex r i) = undefined --z/|z|, simplify?
+    fromInteger int = Complex (fromInteger int) 0
+    (-) (Complex r1 i1) (Complex r2 i2) = Complex (r1-r2) (i1-i2)
 
 -- create a type class
 -- create types that are instances of the type class
