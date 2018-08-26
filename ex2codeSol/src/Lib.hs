4500ba3c9a42a8953c7926878008beafb0967b4f
fishyFrogFace <camillmg@stud.ntnu.no>
2018-08-27 01:15:44 +0200

    Add answer to inhabitation

--- ex2codeSol/src/Lib.hs
+++ ex2codeSol/src/Lib.hs
@@ -9,16 +9,11 @@ module Lib
     , filterPosMany
     ) where
 
-import Prelude hiding (map, take, iterate, sqrt)
+import Prelude hiding (map, take, iterate, sqrt, neg, succ, pred)
 
 -- TASK 1
 -- Parametric polymorphism
 
--- Below are three type signatures. Can you implement them? We
--- say a function or implementation /inhabits/ it's type
--- (signature). How many other inhabitants do these types
--- have? What if we fixed a = Int, does that change your
--- previous answer?
 f0 :: a -> a
 f0 x = x
 
@@ -28,12 +23,25 @@ f1 a _ = a
 f2 :: a -> b -> b
 f2 _ b = b
 
--- rewrite the function "takeInt" so that it
--- accepts a list of any type
--- if you used the built in function "take" on the
--- last assignment, write your own implementation of it
--- hint: you probably don't have to change much
+-- An answer that the types of f0, f1 and f2 have 
+-- one inhabitant is accepted.
+-- Since
+f3 :: a -> a
+f3 = error "another inhabitant"
+-- also type checks, this is also an accepted answer
+-- error is equivalent to undefined and inhabits every type
 
+-- If we fix a = Int, we have more inhabitants to work with, e.g.
+neg :: Int -> Int
+neg x = -x
+
+succ :: Int -> Int
+succ x = x+1
+
+pred :: Int -> Int
+pred x = x-1
+
+--same as ex1, just change [Int] to [a]
 take :: Int -> [a] -> [a]
 take n lst
     | n <= 0    = []
@@ -57,19 +65,9 @@ iterate f x = x : iterate f (f x)
 -- TASK 3
 -- Currying and partial application
 
--- complete the function filterPos
--- that takes a list and returns 
--- a filtered list containing only positive
--- integers
--- use partial application to achieve this
 filterPos :: [Int] -> [Int]
 filterPos lst = filter (>=0) lst
 
--- complete the function filterPosMany
--- that takes a list of lists and returns
--- a list of lists with only positive
--- integers
--- hint: use filterPos and map
 filterPosMany :: [[Int]] -> [[Int]]
 filterPosMany lst = map filterPos lst
 
@@ -96,6 +94,7 @@ approx diff (x:y:xs)
 isPerfSq :: Double -> Bool
 isPerfSq x = doubleIsInt . approx 0.00000001 $ ourInfLst x (x/2)
 
+--tested up to 2000
 accuracy :: Int -> Bool
 accuracy x = take x generated == take x [x^2 | x <- [1..]]
                 where
