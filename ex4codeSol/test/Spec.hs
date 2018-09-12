10c27185dbb4e312b7807dc0ea769dbcf7db4f66
fishyFrogFace <camillmg@stud.ntnu.no>
2018-09-12 11:21:41 +0200

    Remove tests for dupl/negate and add exercise text

--- ex4codeSol/test/Spec.hs
+++ ex4codeSol/test/Spec.hs
@@ -46,10 +46,6 @@ main = hspec $ do
             tokenize ["*", "+", "-", "/"] `shouldBe` [TokOp Mult, TokOp Plus, TokOp Minus, TokOp Div]
         it "tokenizes integers" $ do
             tokenize ["1", "23", "3754924"] `shouldBe` [TokInt 1, TokInt 23, TokInt 3754924]
-        it "tokenizes duplication operator" $ do
-            tokenize ["#"] `shouldBe` [TokOp Dupl]
-        it "tokenizes flip operator" $ do
-            tokenize ["--"] `shouldBe` [TokOp Flip]
         it "recognizes erroneous strings" $ do
             property $ forAll genList $ \n -> tokenize [n] == [TokErr]
         it "returns empty list for empty input" $ do
@@ -60,7 +56,5 @@ main = hspec $ do
             interpret [TokInt 230, TokInt 5, TokOp Plus, TokInt 10, TokOp Minus] `shouldBe` [TokInt 225]
         it "returns empty list for empty input" $ do
             interpret [] `shouldBe` ([] :: [Token])
-        it "flips the sign and duplicates elements" $ do
-            interpret [TokInt 5, TokOp Dupl, TokOp Flip, TokOp Plus] `shouldBe` [TokInt 0]
         it "returns error token for erroneous input" $ do
             interpret [TokErr] `shouldBe` [TokErr]
